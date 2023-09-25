2a674a4b693f73dbb0aaaad2a4b712c112a40895
Enes Ejubovic <enes.ejubovic.bus@gmail.com>
2023-09-25 13:20:17 +0200

    Navbar changes Link

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -32,16 +32,24 @@ export default function Navbar() {
   const currentTheme = theme === "system" ? systemTheme : theme;
   const pathname = usePathname();
   const [navbar, setNavbar] = useState(false);
+
+  const scrollToSection = (sectionId: string) => {
+    const targetSection = document.getElementById(sectionId);
+    if (targetSection) {
+      targetSection.scrollIntoView({
+        behavior: "smooth",
+      });
+    }
+  };
+
   return (
-    <header className="w-full mx-auto  px-4 sm:px-20 fixed top-0 z-50 shadow bg-white dark:bg-stone-900 dark:border-b dark:border-stone-600">
+    <header className="w-full mx-auto px-4 sm:px-20 fixed top-0 z-50 shadow bg-white dark:bg-stone-900 dark:border-b dark:border-stone-600">
       <div className="justify-between md:items-center md:flex">
         <div>
           <div className="flex items-center justify-between py-3 md:py-5 md:block">
-            <Link to="home">
-              <div className="container flex items-center space-x-2 cursor-pointer ">
-                <h2 className="text-2xl font-bold">Enes Ejubovic</h2>
-              </div>
-            </Link>
+            <div className="container flex items-center space-x-2 cursor-pointer">
+              <h2 className="text-2xl font-bold">Enes Ejubovic</h2>
+            </div>
             <div className="md:hidden">
               <button
                 className="p-2 text-gray-700 rounded-md outline-none focus:border-gray-400 focus:border"
@@ -62,21 +70,16 @@ export default function Navbar() {
             <div className="items-center justify-center space-y-8 md:flex md:space-x-6 md:space-y-0">
               {NAV_ITEMS.map((item, idx) => {
                 return (
-                  <Link
+                  <button
                     key={idx}
-                    to={item.page}
-                    className={
-                      "block lg:inline-block text-neutral-900  hover:text-neutral-500 dark:text-neutral-100 cursor-pointer hover:underline"
-                    }
-                    activeClass="active"
-                    spy={true}
-                    smooth={true}
-                    offset={-100}
-                    duration={500}
-                    onClick={() => setNavbar(!navbar)}
+                    className="block lg:inline-block text-neutral-900 hover:text-neutral-500 dark:text-neutral-100 cursor-pointer hover:underline"
+                    onClick={() => {
+                      scrollToSection(item.page);
+                      setNavbar(!navbar);
+                    }}
                   >
                     {item.label}
-                  </Link>
+                  </button>
                 );
               })}
               {currentTheme === "light" ? (
